a1f7b7907db7e0d1ca2764f1bbb17b4983940cdf
AlexStzm <svetozargrey@ya.ru>
2023-07-26 21:15:54 +0300

    Убрана фикстура открытия браузера. Команда browser.open внесена в тела тестов. [id="search"] испарвлены на '#search'

--- tests/test_google_search.py
+++ tests/test_google_search.py
@@ -6,18 +6,16 @@ def teardown_function():
     browser.quit()
 
 @pytest.fixture
-def open_browser(size_browser):
-    browser.open('https://google.com')
-
-@pytest.fixture
 def size_browser():
     browser.config.window_width = 1680
     browser.config.window_height = 1050
 
-def test_valid_search_query(open_browser):
+def test_valid_search_query():
+    browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
-    browser.element('[id="search"]').should(have.text('User-oriented Web UI browser tests in Python'))
+    browser.element('#search').should(have.text('User-oriented Web UI browser tests in Python'))
 
-def test_invalid_search_query(open_browser):
+def test_invalid_search_query():
+    browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('dfgjlrdsjlgjfdsgkjdhfkhglfkdjxlbjfdbmcfnhbkdj').press_enter()
-    browser.element('[id="search"]').should(have.text('Похоже, по вашему запросу нет полезных результатов'))
+    browser.element('#search').should(have.text('Похоже, по вашему запросу нет полезных результатов'))
