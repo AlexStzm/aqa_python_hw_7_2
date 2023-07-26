d251d3564234636e1e0cf0f9cf8afd8522765540
AlexStzm <svetozargrey@ya.ru>
2023-07-26 17:03:28 +0300

    Изменены связи фикстур и добавлена функция закрытия браузера

--- tests/test_google_search.py
+++ tests/test_google_search.py
@@ -2,8 +2,11 @@ import pytest
 from selene.support.shared import browser
 from selene import be, have
 
+def teardown_function():
+    browser.quit()
+
 @pytest.fixture
-def open_browser():
+def open_browser(size_browser):
     browser.open('https://google.com')
 
 @pytest.fixture
@@ -11,10 +14,10 @@ def size_browser():
     browser.config.window_width = 1680
     browser.config.window_height = 1050
 
-def test_valid_search_query(size_browser, open_browser):
+def test_valid_search_query(open_browser):
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('User-oriented Web UI browser tests in Python'))
 
-def test_invalid_search_query(size_browser, open_browser):
+def test_invalid_search_query(open_browser):
     browser.element('[name="q"]').should(be.blank).type('dfgjlrdsjlgjfdsgkjdhfkhglfkdjxlbjfdbmcfnhbkdj').press_enter()
     browser.element('[id="search"]').should(have.text('Похоже, по вашему запросу нет полезных результатов'))
