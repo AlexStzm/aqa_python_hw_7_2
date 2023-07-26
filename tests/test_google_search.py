d7a4965869a335b1c98f59a403d2e95c547631a3
AlexStzm <svetozargrey@ya.ru>
2023-07-26 16:56:01 +0300

    Добавлено 2 фикстуры: открытие браузера и изменение размера браузера

--- tests/test_google_search.py
+++ tests/test_google_search.py
@@ -2,12 +2,19 @@ import pytest
 from selene.support.shared import browser
 from selene import be, have
 
-def test_valid_search_query():
-    browser.open('https://google.com/ru')
+@pytest.fixture
+def open_browser():
+    browser.open('https://google.com')
+
+@pytest.fixture
+def size_browser():
+    browser.config.window_width = 1680
+    browser.config.window_height = 1050
+
+def test_valid_search_query(size_browser, open_browser):
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('User-oriented Web UI browser tests in Python'))
 
-def test_invalid_search_query():
-    browser.open('https://google.com/ru')
+def test_invalid_search_query(size_browser, open_browser):
     browser.element('[name="q"]').should(be.blank).type('dfgjlrdsjlgjfdsgkjdhfkhglfkdjxlbjfdbmcfnhbkdj').press_enter()
     browser.element('[id="search"]').should(have.text('Похоже, по вашему запросу нет полезных результатов'))
