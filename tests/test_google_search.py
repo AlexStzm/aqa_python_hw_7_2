22d771e4ecf5a031c2fef3bfa33a807818ecd5d1
AlexStzm <svetozargrey@ya.ru>
2023-07-29 18:46:56 +0300

    Скорректирован test_google_search.py

--- tests/test_google_search.py
+++ tests/test_google_search.py
@@ -11,10 +11,10 @@ def size_browser():
 
 def test_valid_search_query():
     browser.open('https://google.com')
-    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[name=q]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('#search').should(have.text('User-oriented Web UI browser tests in Python'))
 
 def test_invalid_search_query():
     browser.open('https://google.com')
-    browser.element('[name="q"]').should(be.blank).type('dfgjlrdsjlgjfdsgkjdhfkhglfkdjxlbjfdbmcfnhbkdj').press_enter()
+    browser.element('[name=q]').should(be.blank).type('dfgjlrdsjlgjfdsgkjdhfkhglfkdjxlbjfdbmcfnhbkdj').press_enter()
     browser.element('#search').should(have.text('Похоже, по вашему запросу нет полезных результатов'))
