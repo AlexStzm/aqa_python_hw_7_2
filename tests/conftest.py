97d91da7c485975795015cdb45010f87c6e99172
AlexStzm <svetozargrey@ya.ru>
2023-07-30 14:28:13 +0300

    В файле conftest.py убрана лишняя фикстура

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,5 @@ from selene.support.shared import browser
 def browser_manager():
     browser.config.window_width = 1680
     browser.config.window_height = 1050
-    browser.config.base_url = 'https://chocodiamond.ru'
     yield
     browser.quit()
